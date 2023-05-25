44794d3ba9fa5c77ac2a48ca1fc710252b962ae9
THeHamBurgueler <134538594+THeHamBurgueler@users.noreply.github.com>
2023-05-25 08:57:59 -0400

    Update SeparateFetch.js

--- SeparateFetch.js
+++ SeparateFetch.js
@@ -1,1 +1,1 @@
-
+javascript:(function() {  var url = 'https://raw.githubusercontent.com/projectnomore/Javascript-Console-Emulator-W.I.P/main/SeparateVersion.js';  fetch(url)    .then(response => response.text())    .then(code => {      var script = document.createElement('script');      script.textContent = code;      document.body.appendChild(script);    })    .catch(error => console.error('Error fetching bookmarklet:', error));})();
